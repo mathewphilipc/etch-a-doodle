1a4b0fc6af56dcb6ecd86905292651a8c9eb51a7
Mathew Calkins <mathewpcalkins@gmail.com>
2017-03-10 21:02:43 -0500

    More fiddling, extremely basic functionality for mouseover doodling

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var size = 4;
+
 $(document).ready(function() {
   $(".col1").text("hello other world");
+  $(".box").hover(
+    function() {
+      //$(this).text("Hover effect!");
+    }
+  );
+  $(".box").mouseover(
+    function() {
+      //$(this).text("MO effect!");
+      $(this).css('background-color', 'red');
+      $(this).css('color', 'red');
+    }
+  );
+  $("td").mouseover(
+    function() {
+      //$(this).text("MO effect!");
+      $(this).css('background-color', 'red');
+      $(this).css('color', 'red');
+    }
+  );
 })
