535eb0f8eb3cfb49155d820df32a197ee5762073
Mathew Calkins <mathewpcalkins@gmail.com>
2017-03-11 19:26:19 -0500

    Further fiddlings, didn't get much done. Tomorrow figure out how to insert a variable number of rows and columns from user input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var size = 4;
 
 $(document).ready(function() {
   $(".col1").text("hello other world");
+  $("td").css('background-color','white');
   $(".box").hover(
     function() {
       //$(this).text("Hover effect!");
@@ -16,9 +17,12 @@ $(document).ready(function() {
   );
   $("td").mouseover(
     function() {
+      //var bgColor = $(this).css("background-color");
+      //if (bgColor==="rgb(10,10,10)") {
+      //  $(this).css('background-color', 'red');
+      //}
       //$(this).text("MO effect!");
       $(this).css('background-color', 'red');
-      $(this).css('color', 'red');
     }
   );
 })
